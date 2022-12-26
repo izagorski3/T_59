1e141ab20e6afeeb81e71f33d492bfc322d916ae
izagorski <ivan.zagorski.03@icloud.com>
2022-12-26 06:53:35 -0800

    element dinamicly

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,18 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+ document.body.addEventListener("click", () => {
+    const lenght = 5;
+    let count = 0;
+    for(let i = 0; i < lenght; i++) {
+        let element = document.createElement('article');
+
+        count += lenght;
+        element.setAttribute('id', 'article' + count);
+        document.body.appendChild(element);
+        let text = 'sampleText';
+        document.getElementById('article' + count).innerHTML = text;
+        document.getElementById('article' + count).classList.add('message');
+    }
 });
+  });
